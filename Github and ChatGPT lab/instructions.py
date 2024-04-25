cc2dcbec8c5fbefc9ab14b45206b0dd52dbd961b
gabecler <49306883+gabecler@users.noreply.github.com>
2024-04-25 11:07:38 -0700

    Create instructions.py

---
+++ Github and ChatGPT lab/instructions.py
@@ -0,0 +1,20 @@
+#Your PI says: "Hey we have a collaborator who has fallen off the face of the earth. We did a cool brain study and all that's left is the data analysis. I remember you took that Python class, so you're a data analysis expert, right? Also if you analyze these data you can graduate early."
+
+#Your PI tells you the study was looking at brain responses when participants hear correct and incorrect grammar. You do a little bit of research and decide that you're going to analyze event-related potentials (ERPs).
+
+#Your overall goal is to plot ERPs from all participants individually and an overall average; ERPs should be reactions to (1) syntatically-acceptable sentences vs (2) sentences with syntax errors. You decide for now you'll just analyze the Australian English sentences. Hint: Start with plotting data from the Cz electrode, from one participant.
+
+ 
+
+#For this assignment, do the following:
+
+#Create a github account
+#Fork Dr. Cler's repository: https://github.com/gabecler/SPHSC525_2024Links to an external site.
+#Use ChatGPT to get started. Commit the first try with ChatGPT to your repository. Continue to commit periodically when you have a working version.
+
+#When you are finished, submit to me:
+
+#Link to your (public) repository with your final code
+#A ~1 page summary of your results:
+    #Plot of ERP from Cz for one participant, average of sentences without errors and average of sentences with errors. X-axis should be time and y-axis amplitude. Negative amplitude on y-axis can be down or up (EEG convention), but should be marked. X-axis should have 0 marked as onset of error.
+    #Plot of average ERP from Cz for ALL participants, average of sentences without errors and average of sentences with errors. 
